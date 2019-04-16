df1d1ea77f4ff9f6bc57c92c58f970a7007d1a7c
pranav <pranavverma550@gmail.com>
2019-04-16 08:14:09 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "attributeBinding";
+  cols: number = 3;
+  bdr: number = 1;
+}
